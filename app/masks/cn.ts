36c5237e0750915238f4525eb7808867289140ae
张琪灵 <bash@zxx.sh>
2023-06-25 19:14:50 +0800

    update 版本

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -77,6 +77,7 @@ export const CN_MASKS: BuiltinMask[] = [
       temperature: 1,
       max_tokens: 2000,
       presence_penalty: 0,
+      frequency_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 1000,
